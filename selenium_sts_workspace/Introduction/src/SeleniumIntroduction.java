0d4b857c8b396301573c3f470af91d20cdf0f5fa
Logesh Manoharan <logesh_manoharan@comcast.com>
2022-03-05 22:57:32 +0530

    Chrome App launched using Selenium

--- selenium_sts_workspace/Introduction/src/SeleniumIntroduction.java
+++ selenium_sts_workspace/Introduction/src/SeleniumIntroduction.java
@@ -1,20 +1,26 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
 
 	public static void main(String[] args) {
 		
 		// Invoking Browser
+		// Invoking the ChromDriver.exe
+		/*
+		 * By default, Selenium is looking for the driver at 'webdriver.chrome.driver' (for CHROME)
+		 */
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lmanoh572\\Documents\\Selenium_Web_Driver_UDEMY\\chromedriver\\chromedriver.exe");
 		
 		// 1. Chrome Browser
 		WebDriver chromeWebDriver = new ChromeDriver();
+		chromeWebDriver.get("https://www.google.com");
 		chromeWebDriver.close();
 		
 		// 2. Firefox Browser
-		WebDriver fireFoxDriver = new FirefoxDriver();
-		fireFoxDriver.close();
+//		WebDriver fireFoxDriver = new FirefoxDriver();
+//		fireFoxDriver.close();
 
 	}
 
